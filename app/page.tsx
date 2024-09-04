3dbc7f1312dd539cb0fd03e5caf1de1489a27263
younggraham-t <younggraham.t@gmail.com>
2024-09-04 14:59:03 -0400

    add skills to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,10 @@
-import { GradientBorder } from "./ui/gradient-border";
 import Summary from "./ui/home/summary";
+import Skills from "./ui/home/skills";
 export default function Home() {
   return (
       <div className={`text-center items-center justify-center`}>
           <Summary/>
-          <div className={`flex justify-between items-center`}>
-              <div className={`block w-full`}>
-                  <GradientBorder>
-                  </GradientBorder>
-              </div> 
-              <div className={`block w-full`}>
-                  <GradientBorder>
-                  </GradientBorder>
-              </div>
-              <div className={`block w-full`}>
-                  <GradientBorder>
-                  </GradientBorder>
-              </div>
-          </div>
+          <Skills/>
       </div>
   );
 }
